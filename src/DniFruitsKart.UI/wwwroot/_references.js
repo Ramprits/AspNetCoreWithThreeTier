4369926d5bf0a327bf5f947ccda0d66811cf81c9
Ramprit Sahani <Rampritsahani@outlook.in>
2017-05-20 16:35:53 +0530

    Design Login Page

--- src/DniFruitsKart.UI/wwwroot/_references.js
+++ src/DniFruitsKart.UI/wwwroot/_references.js
@@ -8,5 +8,6 @@
 /// <reference path="js/plugins/moment.min.js" />
 /// <reference path="js/plugins/nouislider.min.js" />
 /// <reference path="js/site.min.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
